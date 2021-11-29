eab360467da6fc9cce250be9925d09de5e2a842c
Roman Goralchuk <gromka.vladyko@gmail.com>
2021-11-29 16:51:57 +0300

    work work

--- hospital-entity/src/main/java/by/itacademy/javaenterprise/goralchuk/entity/client/Client.java
+++ hospital-entity/src/main/java/by/itacademy/javaenterprise/goralchuk/entity/client/Client.java
@@ -5,23 +5,26 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.util.Date;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@MappedSuperclass
 public abstract class Client {
     private String name;
     private String surname;
     private Gender gender;
+    @Temporal(TemporalType.DATE)
     private Date birthday;
 
     @Override
     public String toString() {
         return ", name='" + name +
-                ", surname='" + surname +
-                ", gender=" + gender +
+                "', surname='" + surname +
+                "', gender=" + gender.getCode() +
                 ", birthday=" + birthday;
     }
 }
